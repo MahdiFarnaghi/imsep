4907b7b1d68ed2afb5723bd4fc74139b205e90f2
N. Iranshahi <niranshahi@gmail.com>
2021-03-05 16:22:17 +0330

    sending error for administrator in contact page

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,93 +1,103 @@
-var nodemailer = require('nodemailer');
-
+var nodemailer = require("nodemailer");
 
 /**
  * GET /contact
  */
-exports.contactGet = function(req, res) {
-  var name='';
-  var email='';
-  if(req.user){
-    name=req.user.userName;
-    if(req.user.firstName || req.user.lastName)
-    {
-      name+='(';
-      if(req.user.firstName)
-        name += req.user.firstName +' ';
-      if(req.user.lastName)
-        name += req.user.lastName ;
-      name+=')';   
+exports.contactGet = function (req, res) {
+  var name = "";
+  var email = "";
+  if (req.user) {
+    name = req.user.userName;
+    if (req.user.firstName || req.user.lastName) {
+      name += "(";
+      if (req.user.firstName) name += req.user.firstName + " ";
+      if (req.user.lastName) name += req.user.lastName;
+      name += ")";
     }
-    email=req.user.email;
+    email = req.user.email;
   }
-  res.render('contact', {
-    title: 'Contact',
-    name:name,
-    email:email,
-    message:''
+  res.render("contact", {
+    title: "Contact",
+    name: name,
+    email: email,
+    message: "",
   });
 };
 
 /**
  * POST /contact
  */
-exports.contactPost = function(req, res) {
-  req.assert('name', 'Name cannot be blank').notEmpty();
-  req.assert('email', 'Email is not valid').isEmail();
-  req.assert('email', 'Email cannot be blank').notEmpty();
-  req.assert('message', 'Message cannot be blank').notEmpty();
-  req.sanitize('email').normalizeEmail({ remove_dots: false });
-  req.assert('captcha', 'Captcha check failed').equals(req.session.captcha);
+exports.contactPost = function (req, res) {
+  req.assert("name", "Name cannot be blank").notEmpty();
+  req.assert("email", "Email is not valid").isEmail();
+  req.assert("email", "Email cannot be blank").notEmpty();
+  req.assert("message", "Message cannot be blank").notEmpty();
+  req.sanitize("email").normalizeEmail({ remove_dots: false });
+  req.assert("captcha", "Captcha check failed").equals(req.session.captcha);
 
   var errors = req.validationErrors();
 
   if (errors) {
-    req.flash('error', errors);
-    res.render('contact', {
-      title: 'Contact',
-      name:req.body.name,
-      email:req.body.email,
-      message:req.body.message
+    req.flash("error", errors);
+    res.render("contact", {
+      title: "Contact",
+      name: req.body.name,
+      email: req.body.email,
+      message: req.body.message,
 
       //,csrfToken: req.csrfToken()
     });
     return;
-}
+  }
   var transporter = nodemailer.createTransport({
     service: process.env.EMAIL_SENDER_SERVICE,
     secure: false,
     auth: {
-        user: process.env.EMAIL_SENDER_USERNAME,
-        pass: process.env.EMAIL_SENDER_PASSWORD
+      user: process.env.EMAIL_SENDER_USERNAME,
+      pass: process.env.EMAIL_SENDER_PASSWORD,
     },
     tls: {
-        rejectUnauthorized: false //https://github.com/nodemailer/nodemailer/issues/406
-    }
+      rejectUnauthorized: false, //https://github.com/nodemailer/nodemailer/issues/406
+    },
   });
-  
-    var mailOptions = {
-        from: req.body.name + ' ' + '<' + req.body.email + '>',
-        replyTo :  req.body.email ,
-        to: process.env.CONTACT_TO_EMAIL,
-        subject: '✔ Contact Form | ' + process.env.SITE_NAME,
-        text: req.body.message
-    };
 
-    transporter.sendMail(mailOptions, function (err) {
-      // in case of "Error: self signed certificate in certificate chain", disable antivirus
-      if (err) {
+  var mailOptions = {
+    from: req.body.name + " " + "<" + req.body.email + ">",
+    replyTo: req.body.email,
+    to: process.env.CONTACT_TO_EMAIL,
+    subject: "✔ Contact Form | " + process.env.SITE_NAME,
+    text: req.body.message,
+  };
+
+  transporter.sendMail(mailOptions, function (err) {
+    // in case of "Error: self signed certificate in certificate chain", disable antivirus
+    if (err) {
+      if (
+        res &&
+        res.locals &&
+        res.locals.identity &&
+        res.locals.identity.isAdministrator
+      ) {
         //console.log(error);
-        req.flash('error', {
-          msg: 'Failed to register the message.'
+        req.flash("error", {
+          msg: "Error: " + err.message,
         });
-      }else{
-      req.flash('notify', {
-        type:'success',
-        notify:true,
-        delay:3000, msg: 'Thank you! Your feedback has been submitted.' });
+      } else {
+        //console.log(error);
+        req.flash("error", {
+          msg: "Failed to register the message.",
+        });
       }
+    } else {
+      req.flash("notify", {
+        type: "success",
+        notify: true,
+        delay: 3000,
+        msg: "Thank you! Your feedback has been submitted.",
+      });
+    }
 
-      //  console.log('a ='+a);
-    res.redirect('/contact');
+    //  console.log('a ='+a);
+    res.redirect("/contact");
   });
 };
